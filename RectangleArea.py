d5fcc04e5f079fd1f8a2177a0631b1eb92bc3171
Takayuki Mizutani <mizutani.takayuki@gmail>
2018-05-11 09:50:28 +0900

    first commit

---
+++ RectangleArea.py
@@ -0,0 +1,33 @@
+##RetangleArea=name
+##Scale=number 25000
+##Height=number 20
+##Width=number 20
+
+from qgis.core import QgsVectorLayer, QgsFeature, QgsPoint, QgsRectangle, QgsGeometry, QgsMapLayerRegistry
+from qgis.utils import iface
+from qgis.gui import QgsMessageBar
+
+canvas=iface.mapCanvas()
+if canvas.mapRenderer().destinationCrs().projectionAcronym() != "longlat":
+    epsg = canvas.mapRenderer().destinationCrs().postgisSrid()
+    #QgsMessageLog.logMessage("{}".format(epsg), "name")
+    uri = "Polygon?crs=epsg:" + str(epsg) + "&field=id:integer"
+    mem_layer = QgsVectorLayer(uri, 'rectangular_area', 'memory')
+    prov = mem_layer.dataProvider()
+
+    x=Width*Scale/100
+    y=Height*Scale/100
+
+    p = canvas.extent().center()
+    new_feat = QgsFeature()
+    new_feat.setAttributes([0])
+    p1 = QgsPoint(p[0]-x/2.0, p[1]-x/2.0)
+    p2 = QgsPoint(p[0]+x/2.0, p[1]+x/2.0)
+    new_ext = QgsRectangle(p1,p2)
+    new_tmp_feat = new_ext.asWktPolygon()
+    new_feat.setGeometry(QgsGeometry.fromWkt(new_tmp_feat))
+    prov.addFeatures([new_feat])
+
+    QgsMapLayerRegistry.instance().addMapLayer(mem_layer)
+else:
+    iface.messageBar().pushMessage("Warning", "Change CRS from latlon", level=QgsMessageBar.WARNING)
---
+++ ScalingFeature.py
@@ -0,0 +1,57 @@
+##ScalingFeature=name
+##Scale=number 2.0
+
+from qgis.core import QgsVectorLayer, QgsFeature, QgsPoint, QgsRectangle, QgsGeometry, QgsMapLayerRegistry,QgsMessageLog
+from qgis.utils import iface
+from qgis.gui import QgsMessageBar
+
+
+def scalingPoint(cent,seg,scale):
+    w = scale * (seg[0] - cent[0])
+    h = scale * (seg[1] - cent[1])
+    p = QgsPoint(cent[0] + w, cent[1] + h)
+    return p
+
+canvas=iface.mapCanvas()
+
+layer = iface.activeLayer()
+features = layer.getFeatures()
+epsg = layer.crs().postgisSrid()
+if layer.wkbType()==2:
+    geomtype="LineString"
+elif layer.wkbType()==3:
+    geomtype="Polygon"
+#QgsMessageLog.logMessage("{}".format(geomtype), "name")
+if geomtype=="LineString" or geomtype=="Polygon":
+    uri = geomtype + "?crs=epsg:" + str(epsg)
+    mem_layer = QgsVectorLayer(uri, 'scaling_layer', 'memory')
+    prov = mem_layer.dataProvider()
+
+    fields=layer.dataProvider().fields().toList()
+    mem_layer.dataProvider().addAttributes(fields)
+    mem_layer.updateFields()
+
+    scaling_feature=[]
+    for feature in features:
+        new_feat = QgsFeature()
+        new_feat.setAttributes(feature.attributes())
+        if geomtype == "LineString":
+            geom = feature.geometry().asPolyline()
+        elif geomtype == "Polygon":
+            geom = feature.geometry().asPolygon()[0]
+        cent = feature.geometry().centroid().asPoint()
+        points=[]
+        for seg in geom:
+            p = scalingPoint(cent,seg,Scale)
+            points.append(p)
+        if geomtype=="LineString":
+            new_feat.setGeometry(QgsGeometry.fromPolyline(points))
+        elif geomtype=="Polygon":
+            new_feat.setGeometry(QgsGeometry.fromPolygon([points]))
+        scaling_feature.append(new_feat)
+
+    prov.addFeatures(scaling_feature)
+    QgsMapLayerRegistry.instance().addMapLayer(mem_layer)
+else:
+    iface.messageBar().pushMessage("Warning", "No support layer type. Support LineString or Polygon.",
+                                   level=QgsMessageBar.WARNING)
